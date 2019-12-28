995a9da31ca201d53918584c266329d3713f7bb5
Chinmay <chinmaydash11@gmail.com>
2019-12-28 18:32:20 +0530

    Added fonts

--- lib/app_screen/first_screen.dart
+++ lib/app_screen/first_screen.dart
@@ -1,29 +1,25 @@
-import 'dart:math';
-
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class FirstScreen extends StatelessWidget{
+class First_Screen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-
-    return Material(
-      color: Colors.amberAccent,
-      child: Center(
-        child: Text(
-          generateLuckyNumber(),
-          textDirection: TextDirection.ltr,
-          style: TextStyle(backgroundColor: Colors.blue, fontSize: 40.0),
-        ),
+    // TODO: implement build
+    return Center(
+        child: Container(
+      child: Text(
+        "jhsfjsbd",
+        textDirection: TextDirection.rtl,
+        style: TextStyle(
+            decoration: TextDecoration.none,
+            fontSize: 40.0,
+            backgroundColor: Colors.cyan,
+            color: Colors.amberAccent,
+            fontFamily: "Roboto",
+            fontWeight: FontWeight.w100),
       ),
-    );
+      margin: EdgeInsets.all(20.0),
+      alignment: Alignment.center,
+      color: Colors.purple,
+    ));
   }
-
-  String generateLuckyNumber(){
-    Random random=new Random();
-
-    return "Your lucky number is ${random.nextInt(10)}";
-
-  }
-
 }
--- lib/main.dart
+++ lib/main.dart
@@ -2,32 +2,11 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_app/app_screen/first_screen.dart';
 
-void main() {
-  runApp(MyFutterApp());
-}
-
-class MyFutterApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false ,
-      title: "My Flutter App",
-      home: MySomething(),
-    );
-  }
-}
-
-class MySomething extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    // TODO: implement build
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Chinmay"),
-        elevation: 20.0,
-        centerTitle: true,
-      ),
-      body: FirstScreen()
-    );
-  }
+void main(){
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    title: "My Flutter App",
+    color: Colors.amberAccent,
+    home: First_Screen()
+  ));
 }
